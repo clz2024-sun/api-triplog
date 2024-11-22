7361460232f8f9bc1f37c1462736272f5c1de5ae
clz2024.sun <clz2024.sun@gmail.com>
2024-11-22 20:12:10 +0900

    b

--- src/main/java/com/javaex/controller/HomeController.java
+++ src/main/java/com/javaex/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController implements ErrorController {
 
 	
