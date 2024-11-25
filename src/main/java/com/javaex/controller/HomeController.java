b17b605db15c1335fd6207d17717c8e97a2b172f
clz2024.sun <clz2024.sun@gmail.com>
2024-11-25 16:57:16 +0900

    c

--- src/main/java/com/javaex/controller/HomeController.java
+++ src/main/java/com/javaex/controller/HomeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController implements ErrorController{
 	
 	
-	
     @GetMapping({"/", "/error"})
     public String redirectRoot() {
         System.out.println("HomeController()");
