8c18fee4540dbc87f689dbdbc226dcb8981c84f8
clz2024.sun <clz2024.sun@gmail.com>
2024-11-25 18:58:44 +0900

    b

--- src/main/java/com/javaex/ApiTriplogApplication.java
+++ src/main/java/com/javaex/ApiTriplogApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiTriplogApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTriplogApplication.class, args);
 	}
